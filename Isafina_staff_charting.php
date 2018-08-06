9bc31ed4c941348e58efab5cdbf01e02793c7f0a
jacksonbakari <38470185+jacksonbakari@users.noreply.github.com>
2018-08-06 09:35:18 +0300

    Add files via upload

--- Isafina_staff_charting.php
+++ Isafina_staff_charting.php
@@ -1,56 +1,62 @@
-<?php
-include '../is_receipt/hundphp.php';
- ?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>ISAFINA SCHOOL</title>
-  <link rel="stylesheet" type="text/css" href="staff.css">
-  <link rel="stylesheet" type="text/css" href="../assetss/bootstrap/dist/css/bootstrap.min.css">
-   <script src="../../ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-   <link rel="stylesheet" type="text/css" href="../assetss\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
-</head>
-<body style="background-size:cover; font:20px Montserrat, sans-serif;">
-<?php 
-if (@!$_SESSION['paswd']) {
-  $re="Login first to start transaction";
-  echo '<div class="error"><br>
-        <center><p>'.$re.'</p><br><br>
-        <a href="../is_receipt/login.php"><button class="btn" style="background:lightblue;font-size:20px;">Login</button></a></center>
-      </div>';
-  
-  exit();
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+    margin: 0 auto;
+    max-width: 800px;
+    padding: 0 20px;
 }
- ?>
-<div class="sidenav" style="background:#5F9EA0;">
-  <a class="nav-link" href="..\index.php">Home</a>
-  <a class="nav-link" href="../staffs/index.php">To Staff</a>
-  <a class="nav-link" href="#clients">Clients</a>
-  <a class="nav-link" href="#contact">Contact</a>
-  <a class="nav-link" href="#" data-toggle="modal" data-target="#log"  target="new">Send Text</a>
-  <hr style="background:white">
-    <h6 style="color:white">Online Now</h6>
-  <div class="chip">
-      <img src="../pic/family.jpg" alt="Person" width="96" height="96">
-      John Doe
-  </div>
-<div class="chip">
-  <img src="../pic/family.jpg" alt="Person" width="96" height="96">
-  John Doe
-</div>
-</div>
 
+.container {
+    border: 2px solid #dedede;
+    background-color: #f1f1f1;
+    border-radius: 5px;
+    padding: 10px;
+    margin: 10px 0;
+}
 
-<div class="main">
-  <div class="header">
-  
-<h2>Staff Chat</h2>
-<style type="text/css">
-  p{
-    color:black;
-    font-size:18px;
+.darker {
+    border-color: #ccc;
+    background-color: #ddd;
+}
+
+.container::after {
+    content: "";
+    clear: both;
+    display: table;
+}
+
+.container img {
+    float: left;
+    max-width: 60px;
+    width: 100%;
+    margin-right: 20px;
+    border-radius: 50%;
+}
+
+.container img.right {
+    float: right;
+    margin-left: 20px;
+    margin-right:0;
+}
+
+.time-right {
+    float: right;
+    color: #aaa;
+}
+
+.time-left {
+    float: left;
+    color: #999;
 }
 </style>
+</head>
+<body>
+
+<h2>Chat Messages</h2>
+
 <div class="container">
   <img src="../pic/founder.jpg" alt="Avatar" style="width:100%;">
   <p>Hello. How are you today?</p>
@@ -75,270 +81,5 @@ if (@!$_SESSION['paswd']) {
   <span class="time-left">11:05</span>
 </div>
 
-</div>
-</div>
-
-
-
-<!-- ========to do listttt========= -->
-<style>
-
-/* Include the padding and border in an element's total width and height */
-* {
-  box-sizing: border-box;
-}
-
-/* Remove margins and padding from the list */
-ul {
-  margin: 0;
-  padding: 0;
-}
-
-/* Style the list items */
-ul li {
-  cursor: pointer;
-  position: relative;
-  padding: 12px 8px 12px 40px;
-  list-style-type: none;
-  background: #eee;
-  font-size: 18px;
-  transition: 0.2s;
-  
-  /* make the list items unselectable */
-  -webkit-user-select: none;
-  -moz-user-select: none;
-  -ms-user-select: none;
-  user-select: none;
-}
-
-/* Set all odd list items to a different color (zebra-stripes) */
-ul li:nth-child(odd) {
-  background: #f9f9f9;
-}
-
-/* Darker background-color on hover */
-ul li:hover {
-  background: #ddd;
-}
-
-/* When clicked on, add a background color and strike out text */
-ul li.checked {
-  background: #888;
-  color: #fff;
-  text-decoration: line-through;
-}
-
-/* Add a "checked" mark when clicked on */
-ul li.checked::before {
-  content: '';
-  position: absolute;
-  border-color: #fff;
-  border-style: solid;
-  border-width: 0 2px 2px 0;
-  top: 10px;
-  left: 16px;
-  transform: rotate(45deg);
-  height: 15px;
-  width: 7px;
-}
-
-/* Style the close button */
-.close {
-  position: absolute;
-  right: 0;
-  top: 0;
-  padding: 12px 16px 12px 16px;
-}
-
-.close:hover {
-  background-color: #f44336;
-  color: white;
-}
-
-/* Style the header */
-.header1 {
-  background:#5F9EA0;
-  padding: 30px 10px;
-  color: white;
-  text-align: center;
-  margin:30px auto 0px;
-}
-
-/* Clear floats after the header */
-.header1:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Style the input */
-input {
-  margin: 0;
-  border: none;
-  border-radius: 0;
-  width: 75%;
-  padding: 10px;
-  float: left;
-  font-size: 16px;
-}
-
-/* Style the "Add" button */
-.addBtn {
-  padding: 10px;
-  width: 25%;
-  background: #d9d9d9;
-  color: #555;
-  float: left;
-  text-align: center;
-  font-size: 16px;
-  cursor: pointer;
-  transition: 0.3s;
-  border-radius: 0;
-}
-
-.addBtn:hover {
-  background-color: #bbb;
-}
-.li{
-  width:40%;
-  margin:20px auto 0px;
-  color:black;
-
-}
-</style>
-<div id="myDIV" class="header1" style="width:40%">
-  <h2 style="margin:5px">My To Do List</h2>
-  <input type="text" id="myInput" placeholder="Title...">
-  <span onclick="newElement()" class="addBtn">Add</span>
-</div>
-
-<ul id="myUL" class="li">
-  <li>Hit the gym</li>
-  <li class="checked">Pay bills</li>
-  <li>Meet George</li>
-  <li>Buy eggs</li>
-  <li>Read a book</li>
-  <li>Organize office</li>
-</ul>
-<br>
-<br>
-<script>
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
-
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
-}
-</script>
-
-<!-- Modal 002-->
-  <div id="log" class="modal fade"  role="dialog">
-    <div class="modal-dialog modal-lg" role="document">
-    
-      <!-- Modal content-->
-      <div class="modal-content"style="background:rgb(232, 241, 214)">
-        <div class="modal-header">
-          <div class="row">
-              <h3  style="float:right; color:black;font:20px Montserrat, sans-serif;">Chating here:</h3>
-            </div>
-        </div>
-        <div class="modal-body">
-          <div class="cont">
-            <div class="row">
-              <div class="col-sm-12">
-              <form class="form-outline">
-                  <div class="row">
-                    <div class="input-group">
-                      <label>Staff name /Staff Number:</label>
-                      <input class="form-control" type="text" name="name" id="name" required="">
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="input-group">
-                      <label>Text:</label>
-                      <input class="form-control" type="text" name="text" id="text" required=" " style="height:40px">
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="input-group">
-                      <button class="btn" type="submit" name="send" id="send">Send</button>
-                    </div>
-                  </div>
-                </div>
-              </form>
-              </div>
-          </div>
-          </div>
-
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>
-  </div>
-  </div>
-
-<script src="node_modules/chart.js/dist/Chart.min.js"></script>
-  <script src="node_modules/progressbar.js/dist/progressbar.min.js"></script>
-  <!-- End plugin js for this page-->
-  <!-- inject:js -->
-  <script src="js/misc.js"></script>
-  <script src="js/material.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="js/dashboard.js"></script>
-    <script type="text/javascript" src="../assetss/js/jquery.min.js"></script>
-  <script type="text/javascript" src="../assetss/js/popper.min.js"></script>
-  <script type="text/javascript" src="../assetss/bootstrap/dist/js/bootstrap.min.js"></script>
-  <!-- End custom js for this page-->
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,29 +1,50 @@
-<?php
-include '../is_receipt/hundphp.php';
- ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>ISAFINA SCHOOL</title>
 	<link rel="stylesheet" type="text/css" href="style/ISAFINA.css">
-	<link rel="stylesheet" type="text/css" href="../assetss/bootstrap/dist/css/bootstrap.min.css">
-   <script src="../../ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-   <link rel="stylesheet" type="text/css" href="../assetss\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css">
+	<link rel="stylesheet" type="text/css" href="assetss/bootstrap/dist/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="assetss\fontawesome\web-fonts-with-css\css\fontawesome-all.min.css"> 
+  <!-- <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script> -->
+   <style type="text/css">
+        #myBtn {
+        display: none;
+        position: fixed;
+        bottom: 20px;
+        right: 30px;
+        z-index: 99;
+        font-size: 18px;
+        border: none;
+        outline: none;
+        background-color: red;
+        color: white;
+        cursor: pointer;
+        padding: 15px;
+        border-radius: 4px;
+      }
+
+      #myBtn:hover {
+      background-color: #555;
+}
+.img:hover {
+    -webkit-transform: scaleX(-1);
+    transform: scaleX(-1);
+}
+</style>
+<!-- <script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script> -->
+<!-- 
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script> -->
 </head>
 <body style="background-size:cover; font:20px Montserrat, sans-serif;">
-<?php 
-if (@!$_SESSION['paswd']) {
-  $re="Login first to start transaction";
-  echo '<div class="error"><br>
-        <center><p>'.$re.'</p><br><br>
-        <a href="login.php"><button class="btn" style="background:lightblue;font-size:20px;">Login</button></a></center>
-      </div>';
-  
-  exit();
-}
- ?>
+  <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
   <div class="container-fluid bg-1 text-center">
 	 <div class="header"  style="background:white;width:100%;box-shadow:0 0 10px 0 black;padding:0px;margin:10px auto 0px;color:rgb(44, 99, 13)">
+    <div ></div>
+    <button style="float:right;" id="google_translate_element" class="btn" onclick="googleTranslateElementInit('swahili')">Change Language</button>
     	<center><h3>YOUTH WITH MISSION TANGA-TANZANIA.</h3></center>
     	 <center><h3>(YWAM)</h3></center>
 		  <center>
@@ -38,67 +59,207 @@ if (@!$_SESSION['paswd']) {
 			color:rgb(91, 68, 41);
 		}
 		</style>
-  <ul class="nav nav-tabs">
-    <li class="nav-item">
-      <a class="navbar-brand" href="#">
-        <img src="../pic/ywam_logo.png" alt="logo" style="width:70%;">
-      </a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link active" href="">Home</a>
-    </li>
-    <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Applications</a>
-            <div class="dropdown-menu" aria-labelledby="dropdown01" style="color:white;background:white;" >
-              <a class="dropdown-item" href="aplication/Sregister.php"style="color:black" >Isafina-pre and Primary-school</a>
-                <a class="dropdown-item" href="#"style="color:black" >DTS aplication's</a>
-                <a class="dropdown-item" href="#"style="color:black">Outreach's</a>
-            </div>
-    </li>
-    </li>
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">School analysis</a>
-          <div class="dropdown-menu" aria-labelledby="dropdown01" style="color:white;background:white;" >
-              <a class="dropdown-item" href="chart1.php"style="color:black" >Subjects analysis</a>
-              <a class="dropdown-item" href="chart2.php"style="color:black" >Class analysis</a>
-              <a class="dropdown-item" href="#"style="color:black">Students analysis</a>
-              <a class="dropdown-item" href="#"style="color:black">Staffs analysis</a>
-          </div>
-    </li>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link " href="front/about.php">About us</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="../is_receipt/sesskil.php" >Logout</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="../result/upload.php">Update Results</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="Isafina_staff_charting.php">Staff Chat </a>
-    </li>
-  </ul>
+    <nav class="navbar navbar-expand-md">
+      <button  style="background:lightblue" class="navbar-toggler" data-toggle="collapse" data-target="Isafina">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="Isafina">
+      <a class="navbar-brand"><img src="pic/ywam_logo.png" alt="logo" style="width:60%;"></a>
+        <ul class="nav nav-tabs"  style="width:100%">
+          <li class="nav-item">
+            <a class="nav-link active" href="">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Applications</a>
+                  <div class="dropdown-menu" aria-labelledby="dropdown01" style="color:white;background:white;" >
+                    <a class="dropdown-item" href="aplication/Sregister.php"style="color:black" >Isafina-pre and Primary-school</a>
+                      <a class="dropdown-item" href="#"style="color:black" >DTS aplication's</a>
+                      <a class="dropdown-item" href="#"style="color:black">Outreach's</a>
+                  </div>
+          </li>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Isafina Schools</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01" style="color:white;background:white;" >
+                    <a class="dropdown-item" href="front/isafina_makata.php"style="color:black" >Isafina Mkata</a>
+                    <a class="dropdown-item" href="#"style="color:black" >Isafina Mazingara</a>
+                    <a class="dropdown-item" href="#"style="color:black">Safina Kilimanjaro</a>
+                    <a class="dropdown-item" href="#"style="color:black">Safina Arusha</a>
+                </div>
+          </li>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link " href="front/about.php">About us</a>
+          </li>
+          <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">More Options</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01" style="color:white;background:white;" >
+                    <a class="dropdown-item" href="front/My_reslults.php"style="color:black">My Result</a>
+                    <a class="dropdown-item" href="#"style="color:black" >Progress</a>
+                    <a class="dropdown-item" href="front/opotunities.php"style="color:black">Oportunities</a>
+                    <a class="dropdown-item" href="#"style="color:black">Inovations</a>
+                </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#" data-toggle="modal" data-target="#log"  target="new">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="is_receipt/register.php">Join us</a>
+          </li>
+    </ul>
     </div>
-  </div>
+  </nav>
+</div>
+<img class="img" src="pic/mkatpi.png" style="width:100%;height:400px">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-2"></div>
+		 	<div class="col-sm-8">
+			<center><h1 class="entry-title" style="color:white">Karibu Tanga! Welcome Tanga!</h1></center>
+			</div>
+	</div>
+	</div>
+</div>
+<br>
+<!-- Third Container (Grid) -->
+<!-- <div class="container-fluid bg-3 text-center bg-light">    
+  <h3 class="margin">What You Need To Know About Us?</h3><br>
   <div class="row">
     <div class="col-sm-4">
-      
+      <img  class="img-responsive margin" style="width:70%" alt="Image">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
     </div>
-    <div class="col-sm-4">
-      
+    <div class="col-sm-4"> 
+      <img src="pic/family.jpg" class="img-responsive margin" style="width:80%;height:320px" alt="Image">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+    <div class="col-sm-4"> 
+      <img  class="img-responsive margin" style="width:100%;height:320px" alt="Image">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    </div>
+  </div>
+</div> -->
+
+
+<div class="container">
+    <style type="text/css">
+.column {
+  float: left;
+  width: 33.3%;
+  margin-bottom: 16px;
+  padding: 0 8px;
+}
+
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+}
+.button {
+  border: none;
+  outline: 0;
+  display: inline-block;
+  padding: 8px;
+  color: white;
+  background-color: #000;
+  text-align: center;
+  cursor: pointer;
+  width: 100%;
+}
+
+.button:hover {
+  background-color: #555;
+}
+</style>
+<h2>"Meet The Team"</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="pic/pi6.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="pic/abyi.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="pic/class.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
     </div>
   </div>
 </div>
+</div>
+<div class="container">
+    <div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="pic/staff.jpg" alt="Jane" style="width:100%">
+      <div class="container">
+        <h2>Jane Doe</h2>
+        <p class="title">CEO & Founder</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img src="pic/abyi.jpg" alt="Mike" style="width:100%">
+      <div class="container">
+        <h2>Mike Ross</h2>
+        <p class="title">Art Director</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+      <img src="pic/class.jpg" alt="John" style="width:100%">
+      <div class="container">
+        <h2>John Doe</h2>
+        <p class="title">Designer</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>example@example.com</p>
+        <p><button class="button">Contact</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 <!-- Footer -->
 <footer class="container-fluid bg-4 text-center bg-dark" style="height:130px">
-  <h5 style="color:white">Follow us:</h5>
-  <style type="text/css">
-    i{
-      width:2.5%;
-    }
-  </style>
-    <a href="https://www.instagram.com" target="new"><i class="fab fa-instagram fa-1x" style="color:black"></i></a>
+	<h5 style="color:white">Follow us:</h5>
+	<style type="text/css">
+		i{
+			width:2.5%;
+		}
+	</style>
+		<a href="https://www.instagram.com" target="new"><i class="fab fa-instagram fa-1x" style="color:black"></i></a>
         <a href="https://www.gmail.com" target="new">  <i class="fab fa-google-plus fa-1x" style="color:red"></i></a>
         <a href="https://www.facebook.com" target="new"> <i class="fab fa-facebook-f fa-1x" style="color: blue"></i></a>
         <a href="https://www.twitter.com" target="new"> <i class="fab fa-twitter fa-1x" style="color:rgb(98, 181, 255)"></i></a>
@@ -113,6 +274,83 @@ if (@!$_SESSION['paswd']) {
           </div>
         </div> 
 </footer>
+
+
+
+
+
+<!-- Modal 002-->
+  <div id="log" class="modal fade"  role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+    
+      <!-- Modal content-->
+      <div class="modal-content"style="background:rgb(232, 241, 214)">
+        <div class="modal-header"></div>
+        <div class="modal-body">
+          <div class="container">
+            <div class="row">
+              <h3  style="float:right; color:black;font:20px Montserrat, sans-serif;">Login As:</h3>
+            </div><br><br>
+            <div class="row">
+            	<div class="col-sm-3">
+            		<a href="is_receipt/logins.php"><button type="submit" name="record" id="record" class="btn" style="width:100%;">Student</button></a>
+            	</div>
+            	<div class="col-sm-3">
+            		<a href="is_receipt/logins.php"><button type="submit" name="record" id="record" class="btn" style="width:100%;">DTS Student</button></a>
+            	</div>
+            	<div class="col-sm-3">
+            	<a href="is_receipt/login.php"><button type="submit" name="record" id="record" class="btn" style="width:100%">Staff</button></a>
+            </div>
+            	<div class="col-sm-3">
+            	<a href="is_receipt/logiad.php"><button type="submit" name="record" id="record" class="btn" style="width:100%">Admin</button></a>
+            </div>
+          </div>
+          </div>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+  </div>
+  </div>
+
+
+<!-- ============================another result modale================= -->
+
+<!-- Modal 002-->
+  <div id="result" class="modal fade"  role="dialog">
+    <div class="modal-dialog modal-lg" role="document">
+    
+      <!-- Modal content-->
+      <div class="modal-content"style="background:rgb(232, 241, 214)">
+        <div class="modal-header"></div>
+        <div class="modal-body">
+          
+         
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+<script>
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+</script>
 <script src="node_modules/chart.js/dist/Chart.min.js"></script>
   <script src="node_modules/progressbar.js/dist/progressbar.min.js"></script>
   <!-- End plugin js for this page-->
@@ -122,9 +360,9 @@ if (@!$_SESSION['paswd']) {
   <!-- endinject -->
   <!-- Custom js for this page-->
   <script src="js/dashboard.js"></script>
-    <script type="text/javascript" src="../assetss/js/jquery.min.js"></script>
-  <script type="text/javascript" src="../assetss/js/popper.min.js"></script>
-  <script type="text/javascript" src="../assetss/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assetss/js/jquery.min.js"></script>
+  <script type="text/javascript" src="assetss/js/popper.min.js"></script>
+  <script type="text/javascript" src="assetss/bootstrap/dist/js/bootstrap.min.js"></script>
   <!-- End custom js for this page-->
 </body>
 </html>
